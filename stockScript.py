03f2e8e8b119c600b3d378c60980392c4bebdad2
Ayushmaan Ganotra <ayushmaang@gmail.com>
2018-03-11 08:23:27 -0400

    added stockScript

---
+++ stockScript.py
@@ -0,0 +1,65 @@
+import requests
+import sys
+import time
+
+
+
+def foo(n):
+    #tied to multiprocessing
+    getRecommendationMean()
+    volatility()
+
+def volatility():
+    url = "https://finance.yahoo.com/quote/" + TICKER_SYMBOL + "?p=" + TICKER_SYMBOL
+    r = requests.get(url).text
+    x = r.find("FIFTY_TWO_WK_RANGE-value")
+    start = x+44
+    end = x+61
+    fin = r[start:end].replace("<", "")
+    fin = fin.replace("/", "")
+    fin = fin.replace("t", "")
+    fin = fin.replace("d", "")
+    fin = fin.replace(">", "")
+    fin = fin.replace(",", "")
+    print(fin)
+    middle = fin.find(" - ");
+    print(str(truncate(float(fin[middle+3:])- float(fin[:middle]),2)))
+    changePerc = (float(fin[middle+3:])- float(fin[:middle]))/ float(fin[:middle])
+    print("Change" + str(changePerc))
+    if changePerc > .75:
+        lowRisk.append((TICKER, fin[middle+3:]))
+    if changePerc < .75 and changePerc > .35:
+        medRisk.append((TICKER, fin[middle+3:]))
+    if changePerc < .35:
+        highRisk.append((TICKER, fin[middle+3:]))
+        
+      
+def getRecommendationMean():
+    url = "https://finance.yahoo.com/quote/" + TICKER_SYMBOL + "/analysts?p=" + TICKER_SYMBOL
+    r = requests.get(url).text
+    x = r.find("recommendationMean")
+    print(r[x+27:x+30])
+def truncate(f, n):
+    '''Truncates/pads a float f to n decimal places without rounding'''
+    s = '{}'.format(f)
+    if 'e' in s or 'E' in s:
+        return '{0:.{1}f}'.format(f, n)
+    i, p, d = s.partition('.')
+    return '.'.join([i, (d+'0'*n)[:n]])
+
+sdict = [("Google","GOOG"), ("Go Pro", "GPRO"), ("Snap INC", "Snap"), ("Disney", "DIS"), ("Amazon", "AMZN"), ("NVIDIA", "NVDA"), ("Apple", "AAPL"), ("Microsoft", "MSFT"), ("Facebook", "FB"), ("Twitter", "TWTR"), ("Tesla", "TSLA"), ("Netflix", "NFLX")]
+highRisk = [];
+medRisk = [];
+lowRisk = [];
+TICKER_SYMBOL = "DIS"
+TICKER = "Disney"
+for i in sdict:
+    print(i[0])
+    TICKER_SYMBOL = i[1]
+    TICKER = i[0]
+    foo(10);
+foo(10)
+print(highRisk)
+print(medRisk)
+print(lowRisk)
+
